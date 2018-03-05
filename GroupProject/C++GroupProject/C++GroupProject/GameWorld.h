a6661ca51bfa0ac60d63e42ecbf39bd3dc0fa8b8
Nohbdy <Billen93@outlook.com>
2018-03-05 13:24:29 +0100

    Fixed most of project

---
+++ GroupProject/C++GroupProject/C++GroupProject/GameWorld.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <GLFW\glfw3.h>
+
+#include <iostream>
+
+class GameWorld
+{
+
+	/*  Test anvendelse af SOIL (Kan jeg ikke få til at virke!! T_T )
+	window = windowContext;
+	tmpGO = new GameObject();
+	glEnable(GL_TEXTURE_2D); //Aktivere tektur mapning
+							 //Specificere hvorledes tekture interpoliseres over overflader
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);	*/
+};
+
--- GroupProject/C++GroupProject/irrKlangTest/irrKlang.cpp
+++ GroupProject/C++GroupProject/irrKlangTest/irrKlang.cpp
@@ -20,11 +20,17 @@ irrKlang::~irrKlang()
 int main()
 {
 	ISoundEngine * engine = createIrrKlangDevice(); //Creates engine
+
+	int tmp;
 	
 	//Skriv folder destination fra .exe fil, for at henvise til folder ved .exe filen. Derved skabe bedre overblik.
 	engine->play2D("sounds/Nope.mp3"); //Play sound. Sound is placed in folder with .exe file
-	
-	int tmp;
+
+	getchar();
+	cin.ignore();
+
+	engine->play2D("sounds/Nope.mp3"); //Play sound. Sound is placed in folder with .exe file
+
 
 	cout << "Press 1 + enter to exit";
 	cin >> tmp;
